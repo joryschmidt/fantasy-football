39efe74adf163c23538425750e8bdabf46f9aea6
Jory Schmidt <joryschmidt1988@gmail.com>
2016-10-15 22:43:57 +0000

    further setup TDD

--- karma.conf.js
+++ karma.conf.js
@@ -15,11 +15,13 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      '**/*.js'
     ],
 
 
     // list of files to exclude
     exclude: [
+      './node_modules/**'
     ],
 
 
