18e9479978d0ccfa81f2a881607857d629c8cee0
Jory Schmidt <joryschmidt1988@gmail.com>
2016-11-24 03:55:26 +0000

    finish parse-stats and continue work on casper script

--- scraper/parse-stats.js
+++ scraper/parse-stats.js
@@ -1,21 +1,51 @@
-var webpage = require('webpage');
-
-var parseStats = function(title, body) {
-  var page = webpage.create();
-  page.content = body;
-  page.evaluate(function() {
-    console.log(document.getElementsByTagName('th'));
-  });
+var parseStats = function() {
   
-  // page.includeJS('jquery.min.js', function() {
-  //   page.evaluate(function() {
-  //     console.log('QB Rating', $('.qb-rating').text());
-  //   });
-  // });
+  // this function determines the unique stat types based on the Yahoo data structure
+  var determineStatType = function(classData) {
+    var stat_types = {
+      opponent: 'Opponent',
+      date: 'Date',
+      score: 'Score',
+      105: 'Passing Yards',
+      108: 'Passing Touchdowns',
+      203: 'Rushing Yards',
+      205: 'Rushing Average',
+      206: 'Rushing Long',
+      207: 'Rushing Touchdowns',
+      303: 'Receiving Yards',
+      305: 'Receiving Average',
+      306: 'Receiving Long',
+      309: 'Receiving Touchdowns'
+    };
+    for (var type in stat_types) {
+      var regex = new RegExp(type);
+      if (classData.search(regex) !== -1) return stat_types[type];
+    }
+    return false;
+  };
   
-  // var name = title.match(/^\w+\s\w+/)[0];
-  // var filename = name.replace(/\s/, '_') + '.json';
-  // fs.write('qb/' + filename, data);
+  // this loops through each week and then the stats each week to produce an object for that weeks stats
+  var total_stats = {};
+  var table = document.querySelector('#mediasportsplayergamelog tbody');
+  var games = table.children;
+  for (var i=0; i<games.length; i++) {
+    var game = games[i].children;
+    var statsObj = {};
+    for (var j=0; j<game.length; j++) {
+      var stat = game[j];
+      var stat_type = determineStatType(stat.className);
+      if (stat_type) {
+        if (stat_type === 'Score') statsObj[stat_type] = stat.children[0].innerHTML;
+        else statsObj[stat_type] = stat.innerHTML;
+      } else {
+        var stat_title = stat.getAttribute('title');
+        statsObj[stat_title] = stat.innerHTML;
+      }
+    }
+    total_stats[statsObj['Date']] = statsObj;
+  }
+  return total_stats;
+  
 };
 
 module.exports = parseStats;
--- scraper/scrape.js
+++ scraper/scrape.js
@@ -1,5 +1,5 @@
 var parseStats = require('./parse-stats');
-var webpage = require('webpage');
+var fs = require('fs');
 
 var casper = require('casper').create({
   verbose: true,
@@ -11,7 +11,18 @@ var casper = require('casper').create({
 
 var url = 'http://sports.yahoo.com/nfl/players';
 var links = [];
-var ex_links = ['http://sports.yahoo.com/nfl/players/6760', 'http://sports.yahoo.com/nfl/players/25812'];
+var ex_links = ['http://sports.yahoo.com/nfl/players/25767', 'http://sports.yahoo.com/nfl/players/25812'];
+
+// var position_link_number = ['31', '32', '33', '34', '44'];
+// var position_link = {
+//   '31': 'QB',
+//   '32': 'RB', 
+//   '33': 'WR',
+//   '34': 'TE',
+//   '44': 'K'
+// };
+
+// make an array of the url for each position to loop through
 
 function getLinks(selector) {
   var links = document.querySelectorAll(selector);
@@ -20,50 +31,59 @@ function getLinks(selector) {
   });
 }
 
-// casper.start(url);
+casper.start(url);
 
-// casper.waitForSelector('#yom-league-players', function() {
-//   this.click('a[data-rapid_p="31"]');
-// });
-
-// casper.waitForSelector('.ysprow1', function() {
-//   links = this.evaluate(getLinks, '.ysprow1 a');
-// });
-
-// casper.then(function() {
-//   links = links.concat(this.evaluate(getLinks, '.ysprow2 a'));
-// });
-
-// casper.then(function() {
-//   links.forEach(function(e, index, arr) {
-//     arr[index] = 'http://sports.yahoo.com' + e;
-//   });
-//   links.forEach(function(e, index, arr) {
-//     if(e.search(/teams/) != -1) links.splice(index, 1);
-//   });
-// });
-
-
-casper.start().eachThen(ex_links, function(response) {
-  
-  var page = webpage.create();
-  page.open(response.data, function(status) {
-    var stats = page.evaluate(function() {
-      var hrs = document.querySelectorAll('#mediasportsplayergamelog tbody');
-      
-      return hrs;
-    });
-    console.log('Stats are', stats);
-  });
-  // Try using raw phantomJS with page.open() here instead to parse the html
-  
-  this.thenOpen(response.data, function(response) {
-    parseStats(this.getTitle(), this.getHTML('#mediasportsplayergamelog tbody', true));
+casper.eachThen(position_link, function(response) {
+  var wait_time = Math.random() * 4000 + 4;
+  this.waitForSelector('#yom-league-players', function() {
+    // this.click('a[data-rapid_p=' + response + ']');
+    // this.wait(wait_time);
+    // this.thenOpen(response.data, function() {
+    //   var title = this.getTitle().match(/\w+\s\w+/g);
+    //   var name = title[0];
+    //   var team = title[1];
+    //   var stats = this.evaluate(parseStats);
+    //   stats['Player'] = name;
+    //   stats['Team'] = team;
+    //   stats['Position'] = 'QB';
+    //   fs.write(stats['Position']+'/'+name.replace(/\s/, '_').toLowerCase()+'.json', JSON.stringify(stats));
+    // });
   });
 });
 
+casper.waitForSelector('#yom-league-players', function() {
+  this.click('a[data-rapid_p="31"]');
+});
+
+casper.waitForSelector('.ysprow1', function() {
+  links = this.evaluate(getLinks, '.ysprow1 a, .ysprow2 a');
+});
+
+casper.then(function() {
+  links.forEach(function(e, index, arr) {
+    arr[index] = 'http://sports.yahoo.com' + e;
+  });
+  links.forEach(function(e, index, arr) {
+    if(e.search(/teams/) != -1) links.splice(index, 1);
+  });
+});
+
+
+// casper.start().eachThen(ex_links, function(response) {
+//   this.thenOpen(response.data, function() {
+//     var title = this.getTitle().match(/\w+\s\w+/g);
+//     var name = title[0];
+//     var team = title[1];
+//     var stats = this.evaluate(parseStats);
+//     stats['Player'] = name;
+//     stats['Team'] = team;
+//     stats['Position'] = 'QB';
+//     fs.write('qb/'+name.replace(/\s/, '_').toLowerCase()+'.json', JSON.stringify(stats));
+//   });
+// });
+
 casper.run(function() {
-  // this.echo(links.length + ' links found', 'GREEN_BAR');
-  // this.echo(' - ' + links.join('\n - ')).exit();
-  this.exit();
+  this.echo(links.length + ' links found', 'GREEN_BAR');
+  this.echo(' - ' + links.join('\n - ')).exit();
+  // this.exit();
 });
